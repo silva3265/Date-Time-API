f6b54bca28e79db403f910080f5165b0ab1da720
silva3265 <silva3265@gmail.com>
2024-06-28 14:39:24 -0300

    Desafio: calculadora de parcelas

---
+++ src/desafio_calculadora_parcelas/CalculadoraParcelas.java
@@ -0,0 +1,20 @@
+package desafio_calculadora_parcelas;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculadoraParcelas {
+
+    public List<LocalDate> calcular(LocalDate dataPrimeiraParcela, int quantidadeParcelas) {
+        List<LocalDate> datasParcelas = new ArrayList<>();
+
+        for (int numeroParcela = 1; numeroParcela <= quantidadeParcelas; numeroParcela++) {
+            LocalDate dataProximaParcela = dataPrimeiraParcela.plusMonths(numeroParcela - 1);
+            datasParcelas.add(dataProximaParcela);
+        }
+
+        return datasParcelas;
+    }
+
+}
